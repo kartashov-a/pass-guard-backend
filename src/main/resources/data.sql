4c029134e9ef27cdfe3752a8a0ffb9de3ade252e
kyd3snik <kartashovandrey73@gmail.com>
2021-10-22 21:00:44 +0300

    Add filling database on start

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO PASSWORD_ITEM(NAME, LOGIN, PASSWORD, URL)
+VALUES ('VK', 'test_user', 'test_passoword', 'https://vk.com'),
+       ('Facebook', 'test_user_facebook', 'test_facebook_passoword', null),
+       ('Google', 'test_gmail_user', 'password', null);

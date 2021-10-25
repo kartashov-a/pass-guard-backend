345a7cbe1234eafb595f9188630e96c5e14c166f
kyd3snik <kartashovandrey73@gmail.com>
2021-10-25 07:20:13 +0300

    Add custom error handling

---
+++ src/main/kotlin/com/kyd3snik/app/base/error/ApiError.kt
@@ -0,0 +1,6 @@
+package com.kyd3snik.app.base.error
+
+data class ApiError(
+    val status: Int,
+    val message: String
+)
---
+++ src/main/kotlin/com/kyd3snik/app/base/error/DefaultExceptionHandler.kt
@@ -0,0 +1,23 @@
+package com.kyd3snik.app.base.error
+
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@ControllerAdvice
+class DefaultExceptionHandler : ResponseEntityExceptionHandler() {
+
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatus,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+        val body = ApiError(status.value(), ex.fieldError?.defaultMessage ?: "")
+        return handleExceptionInternal(ex, body, headers, status, request)
+    }
+}
